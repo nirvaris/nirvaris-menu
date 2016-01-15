517154810d85dfb1d339410401ab72bb5f9145d8
JB <juliano.binder@gmail.com>
2016-01-15 16:49:05 +0000

    tag

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='',
+    name='nirvaris-menu',
     version='0.1',
     packages=['menu'],
     include_package_data=True,
