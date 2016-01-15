fb0099294e1404f050f4a79aef269a97735a62df
JB <juliano.binder@gmail.com>
2016-01-15 16:58:45 +0000

    tag

--- menu/templatetags/menu_tag.py
+++ menu/templatetags/menu_tag.py
@@ -8,4 +8,4 @@ register = template.Library()
 @register.inclusion_tag('tag-menu.html')
 def search_form_tag():
     menu_items = MenuItem.objects.exclude(menu_parent__isnull=True).exclude(menu_parent__exact='')
-    return {'menu_items':menu_items, 'dictionary_url': NV_DICTIONARY_URL}
+    return {'menu_items':menu_items}
