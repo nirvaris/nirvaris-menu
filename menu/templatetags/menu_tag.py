2f012fd756e61e9901228a8e68a3a2d93f648de3
Cristiano K Ritzke <cristianokr@gmail.com>
2016-01-15 15:08:31 -0200

    menu

--- menu/templatetags/menu_tag.py
+++ menu/templatetags/menu_tag.py
@@ -6,6 +6,6 @@ from ..models import MenuItem
 register = template.Library()
 
 @register.inclusion_tag('tag-menu.html')
-def search_form_tag():
-    menu_items = MenuItem.objects.exclude(menu_parent__isnull=True).exclude(menu_parent__exact='')
+def menu_tag():
+    menu_items = MenuItem.objects.all()
     return {'menu_items':menu_items}
