409b55c02ff74ffad7a5ba320485828c410bc564
Cristiano K Ritzke <cristianokr@gmail.com>
2016-01-15 15:52:32 -0200

    add html template

--- menu/templatetags/menu_tag.py
+++ menu/templatetags/menu_tag.py
@@ -1,3 +1,5 @@
+import pdb
+
 from django import template
 from django.conf import settings
 
@@ -7,5 +9,7 @@ register = template.Library()
 
 @register.inclusion_tag('tag-menu.html')
 def menu_tag():
-    menu_items = MenuItem.objects.all()
+    menu_items = MenuItem.objects.exclude(menu_parent__isnull=False)
+
+    pdb.set_trace()
     return {'menu_items':menu_items}
