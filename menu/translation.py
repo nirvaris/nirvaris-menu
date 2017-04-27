b0d47aa741f99916716c2a61137dd585155227a5
Cristiano K Ritzke <cristianokr@gmail.com>
2017-04-27 13:33:26 -0300

    remove modaltranslation from app

--- menu/translation.py
+++
@@ -1,7 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from menu.models import MenuItem
-
-class MenuTranslationOptions(TranslationOptions):
-    fields = ('name',)
-
-translator.register(MenuItem, MenuTranslationOptions)
