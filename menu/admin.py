4030749f7ba984748025a279ca960f1c7aa57d0f
JB <juliano.binder@gmail.com>
2017-11-01 17:29:54 +0000

    menu item admin

--- menu/admin.py
+++ menu/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from .models import MenuItem, Resource, IsAdmin
 
-admin.site.register(MenuItem)
+def make_is_staff(modeladmin, request, queryset):
+    queryset.update(is_published=True)
+
+class MenuItemAdmin(admin.ModelAdmin):
+
+    list_filter = ('name', 'url', 'menu_parent', 'css_class',  'display_order', 'is_hidden','is_superuser','is_staff','is_authenticated', 'is_anonymous')
+    list_display = ('name', 'url', 'menu_parent', 'css_class',  'display_order', 'is_hidden','is_superuser','is_staff','is_authenticated', 'is_anonymous')
+    list_editable = ('is_superuser','is_staff','is_authenticated', 'is_anonymous')
+    #actions = [make_published]
+
+admin.site.register(MenuItem, MenuItemAdmin)
 admin.site.register(Resource)
 admin.site.register(IsAdmin)
